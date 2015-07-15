7b56ac172d0fb6bd0faad103698c2b0d9b9465c4
ajcumine <ajcumine@gmail.com>
2015-07-15 10:48:17 +0100

    initial commit

---
+++ spec/fizzbuzz_spec.rb
@@ -0,0 +1,21 @@
+require 'fizzbuzz'
+
+describe 'fizzbuzz' do
+
+  context 'when I play' do
+
+    xit 'outputs 1 for 1' do
+
+    end
+
+    xit 'outputs fizz for 3' do
+
+    end
+
+    xit 'outputs buzz for 5' do
+
+    end
+
+  end
+
+end
